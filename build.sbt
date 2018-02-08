4c410bb0231697d2dd9a7e6bb10860bfb3fba434
peloton <j.peloton@sussex.ac.uk>
2018-02-08 22:50:24 +0100

    Use implicit class to load fits from the SparkSession. Need to translate that to DataFrameReader though...

--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ lazy val root = (project in file(".")).
  settings(
    inThisBuild(List(
      scalaVersion := "2.11.8",
-     version      := "0.1.0"
+     version      := "0.1.0",
+     mainClass in Compile := Some("sparkfits.ReadFits")
    )),
    name := "toto",
    parallelExecution in Test := false,
@@ -13,10 +14,19 @@ lazy val root = (project in file(".")).
    coverageMinimum := 70,
    publishArtifact in Test := false,
    libraryDependencies ++= Seq(
-     "org.apache.spark" %% "spark-core" % "2.1.0",
-     "org.apache.spark" %% "spark-sql" % "2.1.0",
+     "gov.nasa.gsfc.heasarc" % "nom-tam-fits" % "1.15.2",
+     "org.apache.spark" %% "spark-core" % "2.1.0" % "provided",
+     "org.apache.spark" %% "spark-sql" % "2.1.0" % "provided",
      scalaTest % Test
    )
  )
+
+//  // META-INF discarding
+// mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+//    {
+//     case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+//     case x => MergeStrategy.first
+//    }
+// }
 
 //"gov.nasa.gsfc.heasarc" % "nom-tam-fits" % "1.15.2",
--- src/main/scala/sparkfits/ReadFits.scala
+++ src/main/scala/sparkfits/ReadFits.scala
@@ -1,221 +1,236 @@
 package sparkfits
 
-import org.apache.spark.sql.{SparkSession, DataFrame, SQLContext, Row}
-import org.apache.spark.sql.types._
-
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-
-import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
-import nom.tam.fits.{Fits, BinaryTableHDU, BinaryTable}
+import sparkfits.fits._
 
-object ReadFits {
-
-  // Set to Level.WARN is you want verbosity
-  // Logger.getLogger("org").setLevel(Level.OFF)
-  // Logger.getLogger("akka").setLevel(Level.OFF)
-
-  // Initialise Spark context
-  val conf = new SparkConf()
-  val sc = new SparkContext(conf)
-  val sq = new SQLContext(sc)
+object ReadFits extends App {
   val spark = SparkSession
     .builder()
     .getOrCreate()
 
-  // Internal classes
-  val sfu = new SparkFitsUtil
+  val fn = "/Users/julien/Documents/Workspace_postdoc/Planck/COM_PCCS_143_R2.01.fits"
+  val df = spark.fitsFile(fn)
+  df.show()
+  df.printSchema()
 
-  // For implicit conversions like converting RDDs to DataFrames
-  import spark.implicits._
-
-  /**
-    * Returns a block of rows as a Vector of Tuples.
-    * Useful to turn RDD into DF.
-    * /!\ return tuple of Any (you need a conversion later on).
-    *
-    * @param x : nom.tam.fits.Fits
-    *             Instance of Fits.
-    * @param indexHDU : int
-    *             The HDU to be read.
-    * @param offset : int
-    *             Initial position of the cursor (first row)
-    * @param sizeBlock : int
-    *             Number of row to read.
-    * @param nRowMax : int
-    *             Number total of rows in the HDU.
-    */
-  def yieldRows(x : Fits,
-      indexHDU : Int,
-      offset : Int,
-      sizeBlock : Int,
-      nRowMax : Int) = {
-
-    // Start of the block
-    val start = offset * sizeBlock
-
-    // End of the block
-    val stop_tmp = (offset + 1) * sizeBlock - 1
-    val stop = if (stop_tmp < nRowMax - 1) {
-      stop_tmp
-    } else {
-      nRowMax - 1
-    }
-
-    // Yield rows
-    for {
-      i <- start to stop
-    } yield (
-      // Get the data as Array[Table]
-      Array(x.getHDU(indexHDU).asInstanceOf[BinaryTableHDU]
-      // Get ith row as an Array[Object]
-      .getRow(i)
-      // Get each element inside Objects
-      // /!\ Strings are not returned as Object...
-      .map {
-        case x : Array[_] => x.asInstanceOf[Array[_]](0)
-        case x : String => x
-      }
-    // Map to tuple to allow the conversion to DF later on
-    ).map {
-      case x => (x(0).toString, x(1).toString, x(2).toString)
-    }.toList(0)) // Need a better handling of that...
-  }
-
-  /**
-    * Returns a block of row indices as a Vector of Tuples.
-    *
-    * @param offset : int
-    *             Initial position of the cursor (first row)
-    * @param sizeBlock : int
-    *             Number of row to read.
-    * @param nRowMax : int
-    *             Number total of rows in the HDU.
-    */
-  def yieldIdRows(offset : Int, sizeBlock : Int, nRowMax : Int) = {
-
-    // Start of the block
-    val start = offset * sizeBlock
-
-    // End of the block
-    val stop_tmp = (offset + 1) * sizeBlock - 1
-    val stop = if (stop_tmp < nRowMax - 1) {
-      stop_tmp
-    } else {
-      nRowMax - 1
-    }
-
-    // Yield indices
-    for {
-      i <- start to stop
-    } yield (i)
-  }
-
-  def main(args : Array[String]) {
-    // Open the fits
-    val ffits = new Fits(args(0).toString) with Serializable
-
-    // Get the total number of HDUs
-    val nHDUs = sfu.getNHdus(ffits, 0)
-
-    println("Number of HDUs : " + nHDUs.toString)
-
-    // Process only HDU > 0
-    for (indexHDU <- 1 to nHDUs - 1) {
-      println(s"Processing HDU number $indexHDU")
-
-      // Get the hdu
-      val hdu = ffits.getHDU(indexHDU)
-
-      // Check it is a table
-      val isTable = hdu.isInstanceOf[BinaryTableHDU]
-      if (isTable == false) {
-        System.err.println("Work only with BinaryTableHDU!")
-        System.exit(1)
-      }
-
-      val data = hdu.asInstanceOf[BinaryTableHDU]
-
-      val ncols = data.getNCols
-      val nrows = data.getNRows
-      println(s"Table with [col x row] = [$ncols x $nrows]")
-
-      val nBlock = 100
-      val sizeBlock : Int = nrows / nBlock
-      val nParts = 100
-      val col0 = 0
-
-      val it = data.getHeader.iterator
-      val myheader = sfu.getMyHeader(it, "")
-
-      myheader.split(",").foreach(println)
-
-      println(data.getColumnName(0))
-      println(data.getColumnName(1))
-      println(data.getColumnFormat(0))
-      println(data.getColumnFormat(1))
-      val fitsSchema = StructType(
-        List(
-          StructField("ra", DoubleType, false),
-          StructField("dec", DoubleType, false)
-        )
-      )
-
-      def getDF(rdd : RDD[(Any, Int)], fitstype : String, name : String) = {
-        val x = fitstype match {
-          case "1J" => rdd.asInstanceOf[RDD[(Int, Int)]].toDF(name, "id")
-          case "1E" => rdd.asInstanceOf[RDD[(Double, Int)]].toDF(name, "id")
-          case "D" => rdd.asInstanceOf[RDD[(Double, Int)]].toDF(name, "id")
-          case _ => rdd.asInstanceOf[RDD[(Float, Int)]].toDF(name, "id")
-        }
-        x
-      }
-
-      // def recurDF(df : DataFrame, col : Int, colmax : Int) : DataFrame = {
-      //   if (col == colmax + 1) {
-      //     df
-      //   } else {
-      //     val rdd_tmp = sc.parallelize(0 to nBlock - 1, nParts)
-      //       .map(blockid => (blockid, new Fits(args(0).toString) with Serializable ))
-      //       .map(x => yieldRows(x._2, x._1, sizeBlock, nrows).zip(yieldEmptyRows(x._1, sizeBlock)))
-      //       .flatMap(x => x)
-      //
-      //     val fitstype : String = data.getColumnFormat(col)
-      //     val df_tmp = getDF(rdd_tmp, fitstype, col.toString)
-      //
-      //     println(df_tmp.count())
-      //     println(df.join(df_tmp, "id").count())
-      //     recurDF(df.join(df_tmp, "id"), col + 1, colmax)
-      //   }
-      // }
-
-      // Initialisation (1st column)
-      val rdd = sc.parallelize(0 to nBlock - 1, nParts)
-        .map(blockid => (blockid, new Fits(args(0).toString) with Serializable ))
-        .map(x => yieldRows(x._2, indexHDU, x._1, sizeBlock, nrows))
-        .flatMap(x => x)
-
-      val df = rdd.toDF()
-      df.show()
-      df.printSchema()
-      // Get the correct type for the column
-      // Type of the data
-      // val fitstype : String = data.getColumnFormat(col0)
-      // val df = getDF(rdd, fitstype, col0.toString)
-      // val rddtyped = getRDDType(rdd, fitstype)
-      // println("Initial length = " + rddtyped.count().toString)
-
-      // Recursion
-      // val df_tot = recurDF(df, 1, 2)
-
-      // Finalize
-      // println(df_tot.select("1").count())
-      // df_tot.show()
-
-    }
-  }
 }
+
+// object ReadFits {
+//
+//   // Set to Level.WARN is you want verbosity
+//   // Logger.getLogger("org").setLevel(Level.OFF)
+//   // Logger.getLogger("akka").setLevel(Level.OFF)
+//
+//   // Initialise Spark context
+//   val conf = new SparkConf()
+//   val sc = new SparkContext(conf)
+//   val sq = new SQLContext(sc)
+  // val spark = SparkSession
+  //   .builder()
+  //   .getOrCreate()
+//
+//   // Internal classes
+//   val sfu = new SparkFitsUtil
+//
+  // // For implicit conversions like converting RDDs to DataFrames
+  // import spark.implicits._
+//
+  // /**
+  //   * Returns a block of rows as a Vector of Tuples.
+  //   * Useful to turn RDD into DF.
+  //   * /!\ return tuple of Any (you need a conversion later on).
+  //   *
+  //   * @param x : nom.tam.fits.Fits
+  //   *             Instance of Fits.
+  //   * @param indexHDU : int
+  //   *             The HDU to be read.
+  //   * @param offset : int
+  //   *             Initial position of the cursor (first row)
+  //   * @param sizeBlock : int
+  //   *             Number of row to read.
+  //   * @param nRowMax : int
+  //   *             Number total of rows in the HDU.
+  //   */
+  // def yieldRows(x : Fits,
+  //     indexHDU : Int,
+  //     offset : Int,
+  //     sizeBlock : Int,
+  //     nRowMax : Int) = {
+  //
+  //   // Start of the block
+  //   val start = offset * sizeBlock
+  //
+  //   // End of the block
+  //   val stop_tmp = (offset + 1) * sizeBlock - 1
+  //   val stop = if (stop_tmp < nRowMax - 1) {
+  //     stop_tmp
+  //   } else {
+  //     nRowMax - 1
+  //   }
+  //
+  //   // Yield rows
+  //   for {
+  //     i <- start to stop
+  //   } yield (
+  //     // Get the data as Array[Table]
+  //     Array(x.getHDU(indexHDU).asInstanceOf[BinaryTableHDU]
+  //     // Get ith row as an Array[Object]
+  //     .getRow(i)
+  //     // Get each element inside Objects
+  //     // /!\ Strings are not returned as Object...
+  //     .map {
+  //       case x : Array[_] => x.asInstanceOf[Array[_]](0)
+  //       case x : String => x
+  //     }
+  //   // Map to tuple to allow the conversion to DF later on
+  //   ).map {
+  //     case x => (x(0).toString, x(1).toString, x(2).toString)
+  //   }.toList(0)) // Need a better handling of that...
+  // }
+//
+//   def yieldRows2(x : Fits,
+//       indexHDU : Int,
+//       offset : Int,
+//       sizeBlock : Int,
+//       nRowMax : Int) = {
+//
+//     // Start of the block
+//     val start = offset * sizeBlock
+//
+//     // End of the block
+//     val stop_tmp = (offset + 1) * sizeBlock - 1
+//     val stop = if (stop_tmp < nRowMax - 1) {
+//       stop_tmp
+//     } else {
+//       nRowMax - 1
+//     }
+//
+//     val nColMax = 4
+//
+//     // Yield rows
+//     for {
+//       i <- start to stop
+//     } yield (
+//       // Get the data as Array[Table]
+//       Array(x.getHDU(indexHDU).asInstanceOf[BinaryTableHDU]
+//       // Get ith row as an Array[Object]
+//       .getRow(i)
+//       // Get each element inside Objects
+//       // /!\ Strings are not returned as Object...
+//       .map {
+//         case x : Array[_] => x.asInstanceOf[Array[_]](0)
+//         case x : String => x
+//       }
+//     // Map to tuple to allow the conversion to DF later on
+//     ).map { x => Row.fromSeq(x)}.toList(0)) // Need a better handling of that...
+//   }
+//
+//   /**
+//     * Returns a block of row indices as a Vector of Tuples.
+//     *
+//     * @param offset : int
+//     *             Initial position of the cursor (first row)
+//     * @param sizeBlock : int
+//     *             Number of row to read.
+//     * @param nRowMax : int
+//     *             Number total of rows in the HDU.
+//     */
+//   def yieldIdRows(offset : Int, sizeBlock : Int, nRowMax : Int) = {
+//
+//     // Start of the block
+//     val start = offset * sizeBlock
+//
+//     // End of the block
+//     val stop_tmp = (offset + 1) * sizeBlock - 1
+//     val stop = if (stop_tmp < nRowMax - 1) {
+//       stop_tmp
+//     } else {
+//       nRowMax - 1
+//     }
+//
+//     // Yield indices
+//     for {
+//       i <- start to stop
+//     } yield (i)
+//   }
+//
+//   def main(args : Array[String]) {
+//     // Open the fits
+//     val ffits = new Fits(args(0).toString) with Serializable
+//
+//     // Get the total number of HDUs
+//     val nHDUs = sfu.getNHdus(ffits, 0)
+//
+//     println("Number of HDUs : " + nHDUs.toString)
+//
+//     // Process only HDU > 0
+//     for (indexHDU <- 1 to nHDUs - 1) {
+//       println(s"Processing HDU number $indexHDU")
+//
+//       // Get the hdu
+//       val hdu = ffits.getHDU(indexHDU)
+//
+//       // Check it is a table
+//       val isTable = hdu.isInstanceOf[BinaryTableHDU]
+//       if (isTable == false) {
+//         System.err.println("Work only with BinaryTableHDU!")
+//         System.exit(1)
+//       }
+//
+//       val data = hdu.asInstanceOf[BinaryTableHDU]
+//
+//       val ncols = data.getNCols
+//       val nrows = data.getNRows
+//       println(s"Table with [col x row] = [$ncols x $nrows]")
+//
+//       val nBlock = 100
+//       val sizeBlock : Int = nrows / nBlock
+//       val nParts = 100
+//       val col0 = 0
+//
+//       val it = data.getHeader.iterator
+//       val myheader = sfu.getMyHeader(it, "")
+//
+//       myheader.split(",").foreach(println)
+//
+//       println(data.getColumnName(0))
+//       println(data.getColumnName(1))
+//       println(data.getColumnFormat(0))
+//       println(data.getColumnFormat(1))
+//       val fitsSchema = StructType(
+//         List(
+//           StructField("ra", DoubleType, false),
+//           StructField("dec", DoubleType, false)
+//         )
+//       )
+//
+//       // Initialisation (1st column)
+//       val rdd = sc.parallelize(0 to nBlock - 1, nParts)
+//         .map(blockid => (blockid, new Fits(args(0).toString) with Serializable ))
+//         .map(x => yieldRows(x._2, indexHDU, x._1, sizeBlock, nrows))
+//         .flatMap(x => x)
+//
+//       val df = rdd.toDF()
+//       df.show()
+//       df.printSchema()
+//       // Get the correct type for the column
+//       // Type of the data
+//       // val fitstype : String = data.getColumnFormat(col0)
+//       // val df = getDF(rdd, fitstype, col0.toString)
+//       // val rddtyped = getRDDType(rdd, fitstype)
+//       // println("Initial length = " + rddtyped.count().toString)
+//
+//       // Recursion
+//       // val df_tot = recurDF(df, 1, 2)
+//
+//       // Finalize
+//       // println(df_tot.select("1").count())
+//       // df_tot.show()
+//
+//     }
+//   }
+// }
