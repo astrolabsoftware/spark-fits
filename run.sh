ceba2dfce01ee25f7099e2bf0715f579ec8f11cf
Julien <peloton@lal.in2p3.fr>
2018-02-12 09:43:32 +0100

    Rename the final JAR

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ sbt assembly
 FITS=lib/nom-tam-fits-1.15.2.jar
 
 # Parameters
-# fitsfn="/Users/julien/Documents/workspace/myrepos/ScalaTest/gridtest/data/cat29572.fits"
+fitsfn="/Users/julien/Documents/workspace/myrepos/ScalaTest/gridtest/data/cat29572.fits"
 # fitsfn="/Users/julien/Documents/workspace/myrepos/ScalaTest/healpix/data/des_simbad_ra-dec-red_100000.fits"
-fitsfn="/Users/julien/Documents/Workspace_postdoc/Planck/COM_PCCS_143_R2.01.fits"
+#fitsfn="/Users/julien/Documents/Workspace_postdoc/Planck/COM_PCCS_143_R2.01.fits"
 verbosity=false
 
 # Run it!
 spark-submit \
   --master local[*] \
   --class sparkfits.ReadFits \
-  target/scala-2.11/toto-assembly-0.1.0.jar \
+  target/scala-2.11/spark-fits-assembly-0.1.0.jar \
   $fitsfn $verbosity
 
 #target/scala-2.11/toto_2.11-0.1.0.jar
