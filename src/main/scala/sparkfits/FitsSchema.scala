58963864a7e555fab9c00b78eb547e2eddc56fbf
Julien <peloton@lal.in2p3.fr>
2018-02-12 08:57:19 +0100

    Fix Float type case for fits

--- src/main/scala/sparkfits/FitsSchema.scala
+++ src/main/scala/sparkfits/FitsSchema.scala
@@ -1,11 +1,16 @@
 package sparkfits
 
-object fitsSchema {
+import org.apache.spark.sql.types._
+
+import nom.tam.fits.BinaryTableHDU
+
+object FitsSchema {
 
   def ReadMyType(name : String, fitstype : String): StructField = {
     fitstype match {
       case "1J" => StructField(name, IntegerType, false)
-      case "1E" => StructField(name, DoubleType, false)
+      case "1E" => StructField(name, FloatType, false)
+      case "D" => StructField(name, DoubleType, false)
       case _ => StructField(name, StringType, false)
     }
   }
@@ -16,5 +21,10 @@ object fitsSchema {
     else
       ReadMyType(data.getColumnName(col), data.getColumnFormat(col)) :: ListOfStruct(data, col + 1, colmax)
   }
-  
+
+  def getSchema(data : BinaryTableHDU) : StructType = {
+    val ncols = data.getNCols
+    StructType(ListOfStruct(data, 0, ncols))
+  }
+
 }
