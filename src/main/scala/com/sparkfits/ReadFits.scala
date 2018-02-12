f53c7657825db5c5a6bb35c99b93b4c69dba0a41
Julien <peloton@lal.in2p3.fr>
2018-02-12 15:38:04 +0100

    Loop over 2 HDU in the main class + set logger verbosity to lowest...

--- src/main/scala/com/sparkfits/ReadFits.scala
+++ src/main/scala/com/sparkfits/ReadFits.scala
@@ -8,19 +8,25 @@ import org.apache.log4j.Logger
 import com.sparkfits.fits._
 
 object ReadFits {
+  // Set to Level.WARN is you want verbosity
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
   val spark = SparkSession
     .builder()
     .getOrCreate()
 
   def main(args : Array[String]) = {
 
-    val df = spark.readfits
-      .option("datatype", "table")
-      .option("HDU", 1)
-      .option("printHDUHeader", true)
-      .load(args(0).toString)
+    for (hdu <- 1 to 2) {
+      val df = spark.readfits
+        .option("datatype", "table")
+        .option("HDU", hdu)
+        .option("printHDUHeader", true)
+        .load(args(0).toString)
 
-    df.show()
-    df.printSchema()
+      df.show()
+      df.printSchema()
+    }
   }
 }
