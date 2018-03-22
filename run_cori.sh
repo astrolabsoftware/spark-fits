64b62481efc861962e07e082ccb790f0b8415f06
Julien <peloton@lal.in2p3.fr>
2018-03-22 07:40:49 +0100

    Add launcher script for Cori@NERSC

---
+++ run_cori.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -p debug
+#SBATCH -N 6
+#SBATCH -t 00:30:00
+#SBATCH -J sparkFITS
+#SBATCH -C haswell
+
+module load spark
+module load sbt
+
+## SBT Version
+SBT_VERSION=2.11.8
+SBT_VERSION_SPARK=2.11
+
+## Package version
+VERSION=0.2.0
+
+# Package it
+sbt ++${SBT_VERSION} package
+
+# Parameters (put your file)
+fitsfn="/global/cscratch1/sd/plaszczy/colore"
+
+# Run it!
+start-all.sh
+spark-submit \
+  --master $SPARKURL \
+  --driver-memory 15G --executor-memory 20G --executor-cores 17 --total-executor-cores 102 \
+  --class com.sparkfits.ReadFits \
+  target/scala-${SBT_VERSION_SPARK}/spark-fits_${SBT_VERSION_SPARK}-${VERSION}.jar \
+  $fitsfn
+stop-all.sh
+
